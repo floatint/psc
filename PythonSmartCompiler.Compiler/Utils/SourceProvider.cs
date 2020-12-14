f048cb217b39b83a590d9fa69c957eaa6d49061b
Klishin Ilya <klishin-ilya@mail.ru>
2020-12-14 22:17:39 +0300

    SourceProvider class has been added

---
+++ PythonSmartCompiler.Compiler/Utils/SourceProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using Antlr4.Runtime;
+
+namespace PythonSmartCompiler.Compiler.Utils
+{
+    class SourceProvider
+    {
+        public static AntlrInputStream FromFile(string sourcePath, Encoding encoding)
+        {
+            string sourceCode = File.ReadAllText(sourcePath, encoding);
+            return new AntlrInputStream(sourceCode);
+        }
+    }
+}
