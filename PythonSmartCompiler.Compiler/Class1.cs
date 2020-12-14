47a7f834384a099f97ff99a695f1d40dc7c6486d
Klishin Ilya <klishin-ilya@mail.ru>
2020-12-14 13:47:50 +0300

    Initial commit

---
+++ PythonSmartCompiler.Compiler/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PythonSmartCompiler.Compiler
+{
+    public class Class1
+    {
+    }
+}
---
+++ PythonSmartCompiler.Driver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PythonSmartCompiler.Driver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
